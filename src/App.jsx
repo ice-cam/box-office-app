73dd9c3ff55d2601805ba94c735c6e60a29b08d0
Komal <kshekhawat2011@gmail.com>
2023-06-22 21:39:11 +0530

    added navigation and shared layout for home and starred page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Starred from './Pages/Starred';
+import MainLayout from './components/MainLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/starred" element={<Starred />}></Route>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/starred" element={<Starred />}></Route>
+        </Route>
         <Route path="/" element={<div>Not Found</div>}></Route>
         {/* <Route path="/" element={<App />}>
           <Route index element={<Home />} />
