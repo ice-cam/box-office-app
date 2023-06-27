48b39428d9eb0653a0f7b8c79b21fe97d3c25c11
Komal <kshekhawat2011@gmail.com>
2023-06-27 12:31:38 +0530

    installed styled-components. Create a global theme with sc.created Custom radio. used NavLink from react-router to style active link

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,27 @@ import Home from './Pages/Home';
 import Starred from './Pages/Starred';
 import MainLayout from './components/MainLayout';
 import Show from './Pages/Show';
+import { GlobalTheme } from './theme';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/starred" element={<Starred />}></Route>
-          </Route>
+      <GlobalTheme>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/starred" element={<Starred />}></Route>
+            </Route>
 
-          <Route path="/show/:showId/" element={<Show />}></Route>
+            <Route path="/show/:showId/" element={<Show />}></Route>
 
-          <Route path="*" element={<div>Not Found</div>}></Route>
-
-          {/* <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="teams" element={<Teams />}>
-            <Route path=":teamId" element={<Team />} />
-            <Route path="new" element={<NewTeamForm />} />
-            <Route index element={<LeagueStandings />} />
-          </Route>
-        </Route>
-        <Route element={<PageLayout />}>
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/tos" element={<Tos />} />
-        </Route>
-        <Route path="contact-us" element={<Contact />} />
-  */}
-        </Routes>
-      </BrowserRouter>
+            <Route path="*" element={<div>Not Found</div>}></Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalTheme>
     </QueryClientProvider>
   );
 }
