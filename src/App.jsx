c7ccd9b1eef421bbdecb1bc5c7685ba79a249f00
Komal <kshekhawat2011@gmail.com>
2023-06-28 14:02:11 +0530

    deployed the app to github pages . use HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './Pages/Home';
 import Starred from './Pages/Starred';
@@ -12,7 +12,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalTheme>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route element={<MainLayout />}>
               <Route path="/" element={<Home />}></Route>
@@ -23,7 +23,7 @@ function App() {
 
             <Route path="*" element={<div>Not Found</div>}></Route>
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </GlobalTheme>
     </QueryClientProvider>
   );
